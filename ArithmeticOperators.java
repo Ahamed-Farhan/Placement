7d350be8c4d1f5f8ad7849a3fcc91bfda26e669f
Ahamed-Farhan <119913725+Ahamed-Farhan@users.noreply.github.com>
2025-02-12 11:04:55 +0530

    Add files via upload

---
+++ ArithmeticOperators.java
@@ -0,0 +1,11 @@
+class ArithmeticOperators {
+    public static void main(String[] args) {
+        Byte a=100;
+        Byte b=50;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+    }
+}
