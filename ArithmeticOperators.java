7b0671a1532f263fc12e723cb2f5f007853c6fcb
Ahamed-Farhan <119913725+Ahamed-Farhan@users.noreply.github.com>
2025-02-12 11:14:39 +0530

    Update ArithmeticOperators.java

--- ArithmeticOperators.java
+++ ArithmeticOperators.java
@@ -1,11 +1,16 @@
 class ArithmeticOperators {
     public static void main(String[] args) {
-        Byte a=100;
-        Byte b=50;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
-        System.out.println(a%b);
+        byte a=100;
+        byte b=50;
+        byte add = (byte)(a+b);
+        byte sub = (byte)(a-b);
+        byte mul = (byte)(a*b);
+        byte div = (byte)(a/b);
+        byte mod = (byte)(a%b);
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+        System.out.println(mod);
     }
 }
